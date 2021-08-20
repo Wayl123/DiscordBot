9cb2de54db02dda69f5835a4b17333b4d81ec72b
Wayne Lee <waynel1996weiyang@gmail.com>
2021-08-19 20:02:55 -0700

    Implement shop display (can't buy them yet)

--- app.js
+++ app.js
@@ -17,23 +17,19 @@ client.on('message', async (message) => {
 	const args = message.content.slice(prefix.length).trim().split(' ')
 	const command = args.shift().toLowerCase()
 
-  switch(command) {
-    case 'ping':
-      message.channel.send('pong')
-      break
-    case 'createprofile':
-    case 'cp':
-      try {
+  try {
+    switch (command) {
+      case 'ping':
+        message.channel.send('pong')
+        break
+      case 'createprofile':
+      case 'cp':
         await database.createGameProfile({userId: message.author.id})
         message.channel.send('Profile created')
-      } catch(error) {
-        message.channel.send(`Error: ${error.message}`)
-      }
-      break
-    case 'gameprofile':
-    case 'gp':
-      try {
-        let gameProfile = await database.getGameProfile({userId: message.author.id})
+        break
+      case 'gameprofile':
+      case 'gp':
+        const gameProfile = await database.getGameProfile({userId: message.author.id})
         if (!gameProfile) {
           throw new Error('Game profile has not been created, create profile with createprofile command')
         }
@@ -56,15 +52,68 @@ client.on('message', async (message) => {
           profileEmbed.addField('All warships', gameProfile.warships)
         }
         message.channel.send(profileEmbed)
-      } catch(error) {
-        message.channel.send(`Error: ${error.message}`)
-      }
-      break
-    case 'buy':
-      try {
+        break
+      case 'shop':
+        const shop = await database.getShop({userId: message.author.id})
+
+        const shopEmbed = new Discord.MessageEmbed()
+          .setTitle('Shop')
+          .addField('Selected Ship', shop.warship.name)
+          .setFooter('Use \'buy [ID]\' to purchase the upgrade')
+        if (shop.shop.hp_cap) {
+          shopEmbed.addField(
+            `Hp \`${shop.hp}/${shop.shop.hp_cap}\``, 
+            //Cost is only displayed when purchased amount hasn't reach cap yet
+            //Resource only shows up in cost if it is needed
+            `${shop.hp !== shop.shop.hp_cap ? 'Cost: ' +
+              `\$${Math.round(shop.shop.cost.hp_base[0]*(shop.shop.cost.hp_scaling[0]**shop.hp))}` +
+              `${shop.shop.cost.hp_base[1] > 0 ? ` Stone ${Math.round(shop.shop.cost.hp_base[1]*(shop.shop.cost.hp_scaling[1]**shop.hp))}` : ''}` +
+              `${shop.shop.cost.hp_base[2] > 0 ? ` Wood ${Math.round(shop.shop.cost.hp_base[2]*(shop.shop.cost.hp_scaling[2]**shop.hp))}` : ''}` +
+              `${shop.shop.cost.hp_base[3] > 0 ? ` Metal ${Math.round(shop.shop.cost.hp_base[3]*(shop.shop.cost.hp_scaling[3]**shop.hp))}` : ''}`
+              : ''}
+              Effect: +${shop.shop.hp_up}
+              ID: \`hp\`
+            `
+          )
+        }
+        if (shop.shop.atk_cap) {
+          shopEmbed.addField(
+            `Atk \`${shop.atk}/${shop.shop.atk_cap}\``, 
+            //Cost is only displayed when purchased amount hasn't reach cap yet
+            //Resource only shows up in cost if it is needed
+            `${shop.atk !== shop.shop.atk_cap ? 'Cost: ' +
+              `\$${Math.round(shop.shop.cost.atk_base[0]*(shop.shop.cost.atk_scaling[0]**shop.atk))}` +
+              `${shop.shop.cost.atk_base[1] > 0 ? ` Stone ${Math.round(shop.shop.cost.atk_base[1]*(shop.shop.cost.atk_scaling[1]**shop.atk))}` : ''}` +
+              `${shop.shop.cost.atk_base[2] > 0 ? ` Wood ${Math.round(shop.shop.cost.atk_base[2]*(shop.shop.cost.atk_scaling[2]**shop.atk))}` : ''}` +
+              `${shop.shop.cost.atk_base[3] > 0 ? ` Metal ${Math.round(shop.shop.cost.atk_base[3]*(shop.shop.cost.atk_scaling[3]**shop.atk))}` : ''}`
+              : ''}
+              Effect: +${shop.shop.atk_up}
+              ID: \`atk\`
+            `
+          )
+        }
+        if (shop.shop.def_cap) {
+          shopEmbed.addField(
+            `Def \`${shop.def}/${shop.shop.def_cap}\``, 
+            //Cost is only displayed when purchased amount hasn't reach cap yet
+            //Resource only shows up in cost if it is needed
+            `${shop.def !== shop.shop.def_cap ? 'Cost: ' +
+              `\$${Math.round(shop.shop.cost.def_base[0]*(shop.shop.cost.def_scaling[0]**shop.def))}` +
+              `${shop.shop.cost.def_base[1] > 0 ? ` Stone ${Math.round(shop.shop.cost.def_base[1]*(shop.shop.cost.def_scaling[1]**shop.def))}` : ''}` +
+              `${shop.shop.cost.def_base[2] > 0 ? ` Wood ${Math.round(shop.shop.cost.def_base[2]*(shop.shop.cost.def_scaling[2]**shop.def))}` : ''}` +
+              `${shop.shop.cost.def_base[3] > 0 ? ` Metal ${Math.round(shop.shop.cost.def_base[3]*(shop.shop.cost.def_scaling[3]**shop.def))}` : ''}`
+              : ''}
+              Effect: +${shop.shop.def_up}
+              ID: \`def\`
+            `
+          )
+        }
+        message.channel.send(shopEmbed)
+        break
+      case 'buy':
         const buyId = args.shift()
 
-        switch(buyId) {
+        switch (buyId) {
           case 'ship':
             const name = args.shift()
             if(!name) {
@@ -77,12 +126,12 @@ client.on('message', async (message) => {
           default: 
             message.channel.send('Invalid item to buy')
         }
-      } catch(error) {
-        message.channel.send(`Error: ${error.message}`)
-      }
-      break
-    default:
-      message.channel.send('Invalid command')
+        break
+      default:
+        message.channel.send('Invalid command')
+    }
+  } catch (error) {
+    message.channel.send(`Error: ${error.message}`)
   }
 })
 
--- mongoDatabase.js
+++ mongoDatabase.js
@@ -11,6 +11,8 @@ module.exports = async () => {
 
   const game_profiles = db.collection('game_profiles')
   const warships = db.collection('warships')
+  const shop_values = db.collection('shop_values')
+  const shop_status = db.collection('shop_status')
 
   //Game Profile
   //Create
@@ -21,7 +23,7 @@ module.exports = async () => {
     }
     
     return await game_profiles.insertOne({
-      userId
+      userId,
     })
   }
 
@@ -30,7 +32,7 @@ module.exports = async () => {
     const aggregateOptions = [
       {
         $match: {
-          userId: userId
+          userId: userId,
         }
       },
       {
@@ -51,12 +53,22 @@ module.exports = async () => {
         $project: {
           _id: 0,
           'selected._id': 0,
-          'selected.userId': 0
+          'selected.userId': 0,
         }
-      }
+      },
     ]
 
     return await game_profiles.aggregate(aggregateOptions).next()
+  }
+
+  const getSelectedShip = async ({userId}) => {
+    const game_profile = await game_profiles.findOne({userId: userId})
+
+    if (!game_profile.selected) {
+      throw new Error('No ship selected')
+    }
+
+    return game_profile.selected
   }
 
   //Update
@@ -67,12 +79,13 @@ module.exports = async () => {
     }
     
     const newWarship = await createWarship({userId, name})
+    await createShopStatus({warshipId: newWarship.insertedId})
 
     return await game_profiles.findOneAndUpdate(
       {userId: userId},
       {
         $push: {warships: name}, 
-        $set: {selected: newWarship.insertedId}
+        $set: {selected: newWarship.insertedId},
       }
     )
   }
@@ -94,13 +107,82 @@ module.exports = async () => {
       hp: 10,
       atk: 10,
       def: 5,
-      turret: 1
+      turret: 1,
     })
+  }
+
+  //Shop
+  //Create
+  const createShopStatus = async ({warshipId}) => {
+    const status = await shop_status.findOne({warshipId: warshipId})
+    if (status) {
+      throw new Error('Shop status for this ship already exist')
+    }
+
+    return await shop_status.insertOne({
+      warshipId,
+      shopTier: 1,
+      hp: 0,
+      atk: 0,
+      def: 0,
+    })
+  }
+
+  //Get one
+  const getShop = async ({userId}) => {
+    const warshipId = await getSelectedShip({userId})
+
+    const aggregateOptions = [
+      {
+        $match: {
+          warshipId: warshipId,
+        }
+      },
+      {
+        $lookup: {
+          from: 'warships',
+          localField: 'warshipId',
+          foreignField: '_id',
+          as: 'warship'
+        }
+      },
+      {
+        $unwind: {
+          path: '$warship',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $lookup: {
+          from: 'shop_values',
+          localField: 'shopTier',
+          foreignField: 'tier',
+          as: 'shop'
+        }
+      },
+      {
+        $unwind: {
+          path: '$shop',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          'warship._id': 0,
+          'warship.userId': 0,
+          'shop._id': 0,
+        }
+      },
+    ]
+
+    return await shop_status.aggregate(aggregateOptions).next()
   }
 
   return {
     createGameProfile,
     getGameProfile,
-    addNewWarshipToGameProfile
+    addNewWarshipToGameProfile,
+    getShop,
   }
 }
